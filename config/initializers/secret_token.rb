8198cf8e13e778d0dc6d338a2347e3d2f3d39e70
P.S.V.R <pmq2001@gmail.com>
2014-04-22 01:00:00 +0800

    3.2.2->3.2.17

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsDiffTrack3::Application.config.secret_token = '0a7d1ed6ffaa4e4cc70fe39c102df55a6acf73d2b33cd41f5a900cfe8e93c1c047b7e04c5bc5bb17d25df962dee0f9d428df5d9dfb3bedad042221e6afd3efbc'
+RailsDiffTrack3::Application.config.secret_token = 'a90d593e16d7c3165b4b6c347866e4396e67b9caec5e4af0467b854f6053ecccf5369f3c0683d9b70ce13beeec9dc73504480ce1254e80d606f713e22c27cbd4'
