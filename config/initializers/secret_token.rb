7386891386531a9a32544045ca0a3a2833099a57
P.S.V.R <pmq2001@gmail.com>
2014-04-22 11:29:01 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsDiffTrack3::Application.config.secret_token = '0a7d1ed6ffaa4e4cc70fe39c102df55a6acf73d2b33cd41f5a900cfe8e93c1c047b7e04c5bc5bb17d25df962dee0f9d428df5d9dfb3bedad042221e6afd3efbc'
